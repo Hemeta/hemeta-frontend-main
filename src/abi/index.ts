b0dd82f10e38c42ee7e658862c343168303bdb92
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 08:30:05 +0300

    Update index.ts

---
+++ src/abi/index.ts
@@ -0,0 +1,20 @@
+export { abi as HemetaBond } from './HemetaBondDepository.json';
+export { abi as HemetaBondStake } from './HemetaBondStakeDepository.json';
+
+export { abi as MaiReserveContract } from './reserves/MaiContract.json';
+export { abi as HemeMaiReserveContract } from './reserves/HemeMaiContract.json';
+
+export { abi as StakedHemeContract } from './tokens/StakedHemeTokenContract.json';
+export { abi as HemeTokenContract } from './tokens/HemeTokenContract.json';
+export { abi as MAIContract } from './tokens/MAIContract.json';
+
+export { abi as BondingCalcContract } from './BondingCalcContract.json';
+export { abi as StakingContract } from './StakingContract.json';
+export { abi as StakingHelperContract } from './StakingHelperContract.json';
+export { abi as TreasuryContract } from './TreasuryContract.json';
+
+export { abi as IDOContract } from './HemetaHemeIDO.json';
+
+export { abi as HemeCirculatingSupply } from './HemeCirculatingSupply.json';
+
+export { abi as HemeTokenMigrator } from './HemeTokenMigrator.json';
