03a495bd7818899799eda43e0218c6d2eb4bb572
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 20:09:43 +0300

    Update get-market-price.ts

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -1,17 +1,14 @@
 import { BigNumber, ethers } from 'ethers';
 import { getAddresses } from '../constants';
-import { HEMEMaiReserveContract } from '../abi';
+import { HemeMaiReserveContract } from '../abi';
 
 export async function getMarketPrice(
   networkID: number,
   provider: ethers.Signer | ethers.providers.Provider,
 ): Promise<BigNumber> {
   const address = getAddresses(networkID);
-  const pairContract = new ethers.Contract(address.RESERVES.MAI_HEME, HEMEMaiReserveContract, provider);
+  const pairContract = new ethers.Contract(address.RESERVES.MAI_HEME, HemeMaiReserveContract, provider);
   const reserves = await pairContract.getReserves();
-  const [heme, mai] = BigNumber.from(address.MAI_ADDRESS).gt(address.HEME_ADDRESS)
-    ? [reserves[0], reserves[1]]
-    : [reserves[1], reserves[0]];
-  const marketPrice = mai.div(heme);
+  const marketPrice = reserves[1].div(reserves[0]);
   return marketPrice;
 }
