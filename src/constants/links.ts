08288fb2567d173e12e4a03b91950899cf9b4f78
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 21:04:56 +0300

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,9 +1,9 @@
-export const DiscordLink = 'https://discord.gg/hemetaheme';
+export const DiscordLink = 'https://discord.gg/hemeta';
 
-export const TwitterLink = 'https://twitter.com/hemetaheme';
+export const TwitterLink = 'https://twitter.com/hemeta_net';
 
-export const DocsLink = 'https://docs.hemetaheme.finance';
+export const DocsLink = 'https://docs.hemeta.net';
 
-export const GithubLink = 'https://github.com/HemetaHeme';
+export const GithubLink = 'https://github.com/Hemeta';
 
-export const MediumLink = 'https://hemetaheme.medium.com/';
+export const MediumLink = 'https://medium.com/@hemeta';
