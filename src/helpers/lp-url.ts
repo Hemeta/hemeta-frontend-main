707bb3bd0ff33cf575c058ce8ba145f511d1a851
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 08:46:09 +0300

    Update lp-url.ts

---
+++ src/helpers/lp-url.ts
@@ -0,0 +1,6 @@
+import { BondKey } from '../constants';
+import { getBond } from '../constants';
+
+export const lpURL = (name: BondKey, networkID: number): string => {
+  return getBond(name, networkID).dexUrl;
+};
