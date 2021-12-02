a185bf424609fa5103a859803139e26c4b787fe3
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 08:19:22 +0300

    Update index.tsx

---
+++ src/views/Stake/index.tsx
@@ -0,0 +1,397 @@
+import { useState, useCallback, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Grid,
+  Box,
+  Paper,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Tab,
+  Tabs,
+  TabsActions,
+  Zoom,
+  makeStyles,
+} from '@material-ui/core';
+import RebaseTimer from '../../components/RebaseTimer/RebaseTimer';
+import TabPanel from '../../components/TabPanel';
+import { trim } from '../../helpers';
+import { changeStake, changeApproval, claimWarmup } from '../../store/slices/stake-thunk';
+import './stake.scss';
+import { useWeb3Context } from '../../hooks';
+import { IPendingTxn, isPendingTxn, txnButtonText } from '../../store/slices/pending-txns-slice';
+import { Skeleton } from '@material-ui/lab';
+import { IReduxState } from '../../store/slices/state.interface';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiOutlinedInput-root': {
+      borderColor: 'transparent',
+      backgroundColor: theme.palette.background.default,
+    },
+    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+      borderColor: theme.palette.mode.lightGray300,
+    },
+    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: theme.palette.mode.lightGray300,
+    },
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: theme.palette.mode.lightGray300,
+    },
+  },
+}));
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function Stake() {
+  const styles = useStyles();
+  const dispatch = useDispatch();
+  const { provider, address, connect, chainID } = useWeb3Context();
+  const tabsActions = useRef<TabsActions>(null);
+
+  const [view, setView] = useState(0);
+  const [quantity, setQuantity] = useState<string>();
+
+  const isAppLoading = useSelector<IReduxState, boolean>(state => state.app.loading);
+  const currentIndex = useSelector<IReduxState, string>(state => {
+    return state.app.currentIndex;
+  });
+  const fiveDayRate = useSelector<IReduxState, number>(state => {
+    return state.app.fiveDayRate;
+  });
+  const hemeBalance = useSelector<IReduxState, string>(state => {
+    return state.account.balances && state.account.balances.heme;
+  });
+  const sHemeBalance = useSelector<IReduxState, string>(state => {
+    return state.account.balances && state.account.balances.sHeme;
+  });
+  const stakeAllowance = useSelector<IReduxState, number>(state => {
+    return state.account.staking && state.account.staking.hemeStake;
+  });
+  const unstakeAllowance = useSelector<IReduxState, number>(state => {
+    return state.account.staking && state.account.staking.sHemeUnstake;
+  });
+  const warmupBalance = useSelector<IReduxState, string>(state => {
+    return state.account.staking && state.account.staking.warmup;
+  });
+  const canClaimWarmup = useSelector<IReduxState, boolean>(state => {
+    return state.account.staking && state.account.staking.canClaimWarmup;
+  });
+  const stakingRebase = useSelector<IReduxState, number>(state => {
+    return state.app.stakingRebase;
+  });
+  const stakingAPY = useSelector<IReduxState, number>(state => {
+    return state.app.stakingAPY;
+  });
+  const stakingTVL = useSelector<IReduxState, number>(state => {
+    return state.app.stakingTVL;
+  });
+  const pendingTransactions = useSelector<IReduxState, IPendingTxn[]>(state => {
+    return state.pendingTransactions;
+  });
+
+  const setMax = () => {
+    if (view === 0) {
+      setQuantity(hemeBalance);
+    } else {
+      setQuantity(sHemeBalance);
+    }
+  };
+
+  const onSeekApproval = async (token: string) => {
+    await dispatch(changeApproval({ address, token, provider, networkID: chainID }));
+  };
+
+  const onChangeStake = async (action: string) => {
+    // eslint-disable-next-line no-restricted-globals
+    //@ts-ignore
+    if (isNaN(quantity) || quantity === 0 || quantity === '') {
+      // eslint-disable-next-line no-alert
+      alert('Please enter a value!');
+    } else {
+      await dispatch(changeStake({ address, action, value: String(quantity), provider, networkID: chainID }));
+    }
+  };
+
+  const onClaimWarmup = async () => {
+    await dispatch(claimWarmup({ address, provider, networkID: chainID }));
+  };
+
+  const hasAllowance = useCallback(
+    token => {
+      if (token === 'HEME') return stakeAllowance > 0;
+      if (token === 'sHEME') return unstakeAllowance > 0;
+      return 0;
+    },
+    [stakeAllowance],
+  );
+
+  const changeView = (event: any, newView: number) => {
+    setView(newView);
+  };
+
+  const trimmedSHemeBalance = trim(Number(sHemeBalance), 4);
+  const stakingRebasePercentage = trim(stakingRebase * 100, 4);
+  const nextRewardValue = trim(
+    (Number(stakingRebasePercentage) / 100) * (Number(trimmedSHemeBalance) + Number(warmupBalance)),
+    4,
+  );
+
+  useEffect(() => {
+    if (tabsActions.current) {
+      setTimeout(() => tabsActions?.current?.updateIndicator(), 300);
+    }
+  }, [tabsActions]);
+
+  return (
+    <div id="stake-view" className={styles.root}>
+      <Zoom in={true}>
+        <Paper className="ohm-card">
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <div className="card-header">
+                <p className="single-stake-title">
+                  HEME Staking ({String.fromCodePoint(0x1f9a6)}, {String.fromCodePoint(0x1f9a6)})
+                </p>
+                <RebaseTimer />
+              </div>
+            </Grid>
+
+            <Grid item>
+              <div className="stake-top-metrics">
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={4} md={4} lg={4}>
+                    <div className="stake-apy">
+                      <p className="single-stake-subtitle">APY</p>
+                      <Box component="p" color="text.secondary" className="single-stake-subtitle-value">
+                        {stakingAPY ? (
+                          new Intl.NumberFormat('en-US', {
+                            style: 'percent',
+                          }).format(stakingAPY)
+                        ) : (
+                          <Skeleton width="150px" />
+                        )}
+                      </Box>
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12} sm={4} md={4} lg={4}>
+                    <div className="stake-tvl">
+                      <p className="single-stake-subtitle">TVL</p>
+                      <Box component="p" color="text.secondary" className="single-stake-subtitle-value">
+                        {stakingTVL ? (
+                          new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                            maximumFractionDigits: 0,
+                            minimumFractionDigits: 0,
+                          }).format(stakingTVL)
+                        ) : (
+                          <Skeleton width="150px" />
+                        )}
+                      </Box>
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12} sm={4} md={4} lg={4}>
+                    <div className="stake-index">
+                      <p className="single-stake-subtitle">Current Index</p>
+                      <Box component="p" color="text.secondary" className="single-stake-subtitle-value">
+                        {currentIndex ? <>{trim(Number(currentIndex), 3)} sHEME2</> : <Skeleton width="150px" />}
+                      </Box>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div>
+            </Grid>
+
+            <div className="staking-area">
+              {!address ? (
+                <div className="stake-wallet-notification">
+                  <div className="wallet-menu" id="wallet-menu">
+                    <Box bgcolor="hemeta.hemetaBlue" className="app-hemeta-button" onClick={connect}>
+                      <p>Connect Wallet</p>
+                    </Box>
+                  </div>
+                  <p className="desc-text">Connect your wallet to stake HEME2 tokens!</p>
+                </div>
+              ) : (
+                <>
+                  <Box className="stake-action-area">
+                    <Tabs
+                      action={tabsActions}
+                      centered
+                      value={view}
+                      indicatorColor="primary"
+                      className="stake-tab-buttons"
+                      onChange={changeView}
+                      aria-label="stake tabs"
+                    >
+                      <Tab label="Stake" {...a11yProps(0)} />
+                      <Tab label="Unstake" {...a11yProps(0)} />
+                    </Tabs>
+
+                    <Box className="stake-action-row" display="flex" alignItems="center">
+                      <FormControl className="ohm-input" variant="outlined" color="primary">
+                        <InputLabel htmlFor="amount-input"></InputLabel>
+                        <OutlinedInput
+                          id="amount-input"
+                          type="number"
+                          placeholder="Amount"
+                          className="stake-input"
+                          value={quantity}
+                          onChange={e => setQuantity(e.target.value)}
+                          labelWidth={0}
+                          endAdornment={
+                            <InputAdornment position="end">
+                              <div onClick={setMax} className="stake-input-btn">
+                                <p>Max</p>
+                              </div>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+
+                      <TabPanel value={view} index={0} className="stake-tab-panel">
+                        <div className="stake-tab-buttons-group">
+                          {address && hasAllowance('HEME') ? (
+                            <Box
+                              className="stake-tab-panel-btn"
+                              bgcolor="hemeta.hemetaBlue"
+                              onClick={() => {
+                                if (isPendingTxn(pendingTransactions, 'staking')) return;
+                                onChangeStake('stake');
+                              }}
+                            >
+                              <p>{txnButtonText(pendingTransactions, 'staking', 'Stake')}</p>
+                            </Box>
+                          ) : (
+                            <Box
+                              className="stake-tab-panel-btn"
+                              bgcolor="hemeta.hemetaBlue"
+                              onClick={() => {
+                                if (isPendingTxn(pendingTransactions, 'approve_staking')) return;
+                                onSeekApproval('HEME');
+                              }}
+                            >
+                              <p>{txnButtonText(pendingTransactions, 'approve_staking', 'Approve')}</p>
+                            </Box>
+                          )}
+                          {canClaimWarmup && (
+                            <Box
+                              className="stake-tab-panel-btn"
+                              bgcolor="hemeta.hemetaBlue"
+                              onClick={() => {
+                                if (isPendingTxn(pendingTransactions, 'claimWarmup')) return;
+                                onClaimWarmup();
+                              }}
+                            >
+                              <p>{txnButtonText(pendingTransactions, 'claimWarmup', 'Claim Warmup')}</p>
+                            </Box>
+                          )}
+                        </div>
+                      </TabPanel>
+
+                      <TabPanel value={view} index={1} className="stake-tab-panel">
+                        {address && hasAllowance('sHEME') ? (
+                          <Box
+                            className="stake-tab-panel-btn"
+                            bgcolor="hemeta.hemetaBlue"
+                            onClick={() => {
+                              if (isPendingTxn(pendingTransactions, 'unstaking')) return;
+                              onChangeStake('unstake');
+                            }}
+                          >
+                            <p>{txnButtonText(pendingTransactions, 'unstaking', 'Unstake HEME2')}</p>
+                          </Box>
+                        ) : (
+                          <Box
+                            className="stake-tab-panel-btn"
+                            bgcolor="hemeta.hemetaBlue"
+                            onClick={() => {
+                              if (isPendingTxn(pendingTransactions, 'approve_unstaking')) return;
+                              onSeekApproval('sHEME');
+                            }}
+                          >
+                            <p>{txnButtonText(pendingTransactions, 'approve_unstaking', 'Approve')}</p>
+                          </Box>
+                        )}
+                      </TabPanel>
+                    </Box>
+
+                    <div className="help-text">
+                      {address && ((!hasAllowance('HEME') && view === 0) || (!hasAllowance('sHEME') && view === 1)) && (
+                        <p className="text-desc">
+                          Note: The "Approve" transaction is only needed when staking/unstaking for the first time;
+                          subsequent staking/unstaking only requires you to perform the "Stake" or "Unstake"
+                          transaction.
+                        </p>
+                      )}
+                    </div>
+                  </Box>
+
+                  <div className={`stake-user-data`}>
+                    {Number(warmupBalance) > 0 && (
+                      <div className="data-row">
+                        <p className="data-row-name-warmup">Your Staked Balance in warmup</p>
+                        <p className="data-row-value">
+                          {isAppLoading ? <Skeleton width="80px" /> : <>{trim(Number(warmupBalance), 4)} HEME2</>}
+                        </p>
+                      </div>
+                    )}
+
+                    <div className="data-row">
+                      <p className="data-row-name">Your Balance</p>
+                      <p className="data-row-value">
+                        {isAppLoading ? <Skeleton width="80px" /> : <>{trim(Number(hemeBalance), 4)} HEME2</>}
+                      </p>
+                    </div>
+                    <div className="data-row">
+                      <p className="data-row-name">Your Staked Balance</p>
+                      <p className="data-row-value">
+                        {isAppLoading ? (
+                          <Skeleton width="80px" />
+                        ) : (
+                          <>{new Intl.NumberFormat('en-US').format(Number(trimmedSHemeBalance))} sHEME2</>
+                        )}
+                      </p>
+                    </div>
+
+                    <div className="data-row">
+                      <p className="data-row-name">Next Reward Amount</p>
+                      <p className="data-row-value">
+                        {isAppLoading ? <Skeleton width="80px" /> : <>{nextRewardValue} sHEME2</>}
+                      </p>
+                    </div>
+
+                    <div className="data-row">
+                      <p className="data-row-name">Next Reward Yield</p>
+                      <p className="data-row-value">
+                        {isAppLoading ? <Skeleton width="80px" /> : <>{stakingRebasePercentage}%</>}
+                      </p>
+                    </div>
+
+                    <div className="data-row">
+                      <p className="data-row-name">ROI (5-Day Rate)</p>
+                      <p className="data-row-value">
+                        {isAppLoading ? <Skeleton width="80px" /> : <>{trim(Number(fiveDayRate) * 100, 4)}%</>}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          </Grid>
+        </Paper>
+      </Zoom>
+    </div>
+  );
+}
+
+export default Stake;
