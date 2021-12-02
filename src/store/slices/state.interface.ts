180339b05dc7a023475f0a487adbd2f135149ad4
Hemeta <94857054+Hemeta@users.noreply.github.com>
2021-12-02 08:52:31 +0300

    Update state.interface.ts

---
+++ src/store/slices/state.interface.ts
@@ -0,0 +1,17 @@
+import { IPendingTxn } from './pending-txns-slice';
+import { IAccount } from './account-slice';
+import { IApp } from './app-slice';
+import { IBond } from './bond-slice';
+import { IWhitelist } from './whitelist-slice';
+import { IDOState } from './ido-slice';
+import { MigrationState } from './migrate-slice';
+
+export interface IReduxState {
+  pendingTransactions: IPendingTxn[];
+  account: IAccount;
+  app: IApp;
+  bonding: IBond;
+  whitelist: IWhitelist;
+  ido: IDOState;
+  migrate: MigrationState;
+}
